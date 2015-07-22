36d4a03453a319caf0902688559675de884ec15e
baltasarq <baltasarq@gmail.com>
2015-07-22 22:53:17 +0200

    Got rid of designer for DlgCsvExport

--- Gui/DlgCsvExport.cs
+++ Gui/DlgCsvExport.cs
@@ -1,5 +1,3 @@
-using Gtk;
-using System;
 using Core = Colorado.Core;
 
 namespace Colorado.Gui
@@ -8,6 +6,7 @@ namespace Colorado.Gui
 		public DlgCsvExport(Gtk.Window parent, Core.CsvDocument doc)
 		{
 			this.Build();
+            this.ShowAll();
 			
 			this.Title = parent.Title + " export as CSV";
 			this.Icon = parent.Icon;
@@ -37,5 +36,31 @@ namespace Colorado.Gui
 		public bool SurroundWithDoubleQuotes {
 			get { return this.chkQuotes.Active; }
 		}
+
+        private void Build() {
+            var hBoxDelimiter = new Gtk.HBox( false, 2 );
+
+            // Delimiter
+            this.lblDelimiter = new Gtk.Label( "Delimiter:" );
+            this.cmbDelimiter = new Gtk.ComboBoxEntry( new string[] { ",", ";" } );
+            hBoxDelimiter.PackStart( this.lblDelimiter, false, false, 5 );
+            hBoxDelimiter.PackStart( this.cmbDelimiter, true, true, 5 );
+
+            // Chk quotes
+            this.chkQuotes = new Gtk.CheckButton( "_Enclose text with quotes" );
+
+            // Layout
+            this.VBox.PackStart( hBoxDelimiter );
+            this.VBox.PackStart( this.chkQuotes );
+
+            // Buttons
+            this.AddButton( Gtk.Stock.Cancel, Gtk.ResponseType.Cancel );
+            this.AddButton( Gtk.Stock.Ok, Gtk.ResponseType.Ok );
+            this.DefaultResponse = Gtk.ResponseType.Ok;
+        }
+
+        private Gtk.Label lblDelimiter;
+        private Gtk.ComboBoxEntry cmbDelimiter;
+        private Gtk.CheckButton chkQuotes;
 	}
 }
