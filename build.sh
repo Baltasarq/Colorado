fb5251e5809f3016db9f64ef398d2064403d5d01
baltasarq <baltasarq@gmail.com>
2025-07-03 16:42:18 +0200

    Imrpoves building

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-dotnet publish Colorado/Colorado.csproj -r linux-x64 -p:PublishSingleFile=true --self-contained false
-cp Colorado/bin/Release/net9.0/linux-x64/publish/Colorado dist/
+ilrepack /out:dist/Colorado.dll Colorado/bin/Release/net9.0/Colorado.dll Colorado/bin/Release/net9.0/*.dll
