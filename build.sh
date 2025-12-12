019ca02e72b4db7ca10932f154d5222c91368c55
baltasarq <baltasarq@gmail.com>
2025-12-12 16:22:41 +0100

    Bumps to .net 10

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-ilrepack /out:dist/Colorado.dll Colorado/bin/Release/net9.0/Colorado.dll Colorado/bin/Release/net9.0/*.dll
+dotnet build -c:Release Colorado
+ilrepack /out:dist/Colorado.dll Colorado/bin/Release/net10.0/Colorado.dll Colorado/bin/Release/net10.0/*.dll
